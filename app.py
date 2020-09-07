9d2fa981db26badf70a3ff227d22253012ae54ac
lokpun03 <smilok03@gmail.com>
2020-09-07 14:42:59 +0545

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	mero_test = str(name) + " Please verify the correct URL" + data_type_name + printlok
+	mero_test = str(name) + " Please verify the correct URL" 
 	return render_template('index.html', name=mero_test)
 
 
