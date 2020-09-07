669ee9ccd6ffe4f160a8a4f133715006ffa45ebe
lokpun03 <smilok03@gmail.com>
2020-09-07 13:18:13 +0545

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	name=name
-  	return render_template('index.html', name=name)
+	return render_template('index.html', name=name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
