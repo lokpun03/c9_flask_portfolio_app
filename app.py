b085ced9c74f8abb722fe2c69e14a26318c70781
lokpun03 <smilok03@gmail.com>
2020-09-07 11:22:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name
+	new_name = name + "Please verify the valid URL"
   	return render_template('index.html', name=new_name)
 
 
